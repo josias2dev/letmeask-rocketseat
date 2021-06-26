12e2bf35dcdcd358d6a40a560580a02546b907a8
josias <josiasr.ribeiro@gmail.com>
2021-06-26 14:10:20 -0300

    Full project

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+import { Room } from './pages/Room';
+
+import { AuthContextProvider } from './context/AuthContext';
+
+import './styles/global.scss';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
